682c7b0fff00877ee6997526365816b45858b9af
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2024-10-25 21:40:34 -0400

    add creation of file or directory, do not allow reserved characters or names

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -33,7 +33,7 @@
           </li>
         </template>
         <li class="file-item hidden" id="temp-file-name">
-          <input type="text" @blur="hideInput()" @keypress.enter="createFileOrDirectory(tempFileName)" @change="validateFileInput()" v-model="tempFileName">
+          <input type="text" id="temp-input" @blur="hideInput()" @keypress.enter="createFileOrDirectory(tempFileName)" @input="validateFileInput()" v-model="tempFileName">
         </li>
       </ul>
 
@@ -74,7 +74,7 @@ export default {
       focusTab: null,
       componentFocus: null,
       tempFileName: null,
-
+      isTempNameValid: true,
     }
   },
   components:{
@@ -83,22 +83,6 @@ export default {
   computed: {
   },
   methods: {
-    createFileOrDirectory(inputName){
-
-      console.log(inputName);
-
-      this.hideInput()
-
-    },
-    validateFileInput(){
-
-    },
-    hideInput(){
-      let inputField = document.getElementById('temp-file-name')
-      inputField.classList.add('hidden')
-      this.tempFileName = null
-      this.tabManager[0].focus()
-    },
     ipcHandle(){
       window.Electron.ipcRenderer.send('ping')
     },
@@ -250,21 +234,26 @@ export default {
           if(this.componentFocus == 'list'){
             if(keyevent.ctrlKey & keyevent.shiftKey){
               
+              document.getElementById('temp-file-name').classList.remove('hidden')
+              document.getElementById('temp-input').focus()
             }
           }
           break;
         case '1':
+        if(keyevent.ctrlKey & keyevent.shiftKey){
           this.focusAppComponent(0)
+        }
           break;
         case '2':
+        if(keyevent.ctrlKey & keyevent.shiftKey){
           this.focusAppComponent(1)
+        }
           break;
         case '3':
-          console.log(3);
+        if(keyevent.ctrlKey & keyevent.shiftKey){
           this.focusAppComponent(2)
+        }
           break;
-
-      
         default:
           break;
       }
@@ -293,6 +282,80 @@ export default {
       if(component == 2){
         this.tabManager[0].focus()
       }
+    },
+    //! AGREGAR MENSAJE DE ERROR POR AHORA SOLO NO TE DEJA CREAR EL ARCHIVO / DIRECTORIO
+    createFileOrDirectory(inputName){
+      
+
+      let notValidFileNames = ['CON','PRN','AUX','NUL','COM1','COM2','COM3','COM4','COM5','COM6','COM7','COM8','COM9','COM0','LPT1','LPT2','LPT3','LPT4','LPT5','LPT6','LPT7','LPT8','LPT9','LPT0']
+      
+      for (let i = 0; i < notValidFileNames.length; i++) {
+        if(this.tempFileName.includes('.')){
+          let tempname = this.tempFileName.split('.')[0]
+
+          if(this.tempFileName.includes(notValidFileNames[i])){
+            console.log('Invalid character found');
+            this.isTempNameValid = false
+            return
+          }
+        }else{
+          if(this.tempFileName.includes(notValidFileNames[i])){
+            console.log('Invalid character found');
+            this.isTempNameValid = false
+            return
+          }
+        }
+      }
+
+      console.log(inputName);
+      if(!this.isTempNameValid) {
+        console.log('nombre no valido');
+        return
+      }
+
+      console.log(inputName);
+      let filepath = path.join(this.currentDir,inputName)
+
+      if(this.tempFileName.includes('.')){
+        fs.writeFile(filepath, '', {flag: 'wx'}, (err)=>{
+          if(err) console.log('Error found on file>>: ', err);
+        })
+      }else{
+        fs.mkdir(filepath, (err)=>{
+          if(err) console.log('Error found on dir>>: ', err);
+        })
+      }
+
+      this.hideInput()
+      this.fileList = this.getDirInfo(this.currentDir)
+
+    },
+    //! AGREGAR MENSAJE DE ERROR POR AHORA SOLO NO TE DEJA CREAR EL ARCHIVO / DIRECTORIO
+    validateFileInput(){
+      
+      
+      
+      let notValidChars = '<>:"/|?*'
+      notValidChars = notValidChars.split('')
+      notValidChars.push('\\')
+
+      for (let i = 0; i < notValidChars.length; i++) {
+        if(this.tempFileName.includes(notValidChars[i])){
+          console.log('Invalid character found');
+          this.isTempNameValid = false
+          return
+        }
+      }
+
+      if (!this.isTempNameValid) {
+        this.isTempNameValid = true
+      }
+      
+    },
+    hideInput(){
+      let inputField = document.getElementById('temp-file-name')
+      inputField.classList.add('hidden')
+      this.tempFileName = null
     },
     
 
